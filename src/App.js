7c04975efeee38c82a3bb775fca515f07d29ffbc
rohitdasari0424 <rohitdasari75@gmail.com>
2024-06-18 19:31:54 -0400

    Starting Profile page - created balance display

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+import ToggleEye from './Components/ToggleEye';
 
 function App() {
   return (
-    <div className="App">
-      <p> Welcome to the crypto exchange. </p>
+    <div className="App bg-slate-100 min-w-full min-h-screen pt-10 pb-10">
+      <div className="bg-white w-full max-w-5xl p-6 ml-auto mr-auto">
+        <div>
+          <div className='flex p-4'>
+            <div className='flex flex-col'>
+              <span className='text-xl font-extralight balance-title'> Balance </span>
+              <div className='mt-4 flex justify-center items-center'>
+                <span className='font-normal mr-4 balance-amount'> $150,000 </span>
+                <ToggleEye />
+                
+              </div>
+            </div>
+            <div className='ml-auto'>
+              <button className='px-4 py-1 font-sans button-border mr-1'>Deposit</button>
+              <button className='px-4 py-1 font-sans button-border border-opacity-30 ml-1'>Withdraw</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
