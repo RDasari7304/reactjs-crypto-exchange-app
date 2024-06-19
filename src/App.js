44b7ff758f5a4a944ac3890cc3cf6f685669b3d4
rohitdasari0424 <rohitdasari75@gmail.com>
2024-06-19 01:53:04 -0400

    Integrated recharts to create line graph of user account balance

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.css';
 import ToggleEye from './Components/ToggleEye';
+import Graph from './Components/Graph';
+import { subDays } from 'date-fns';
 
 function App() {
+  const data = []
+  
+  for(let i = 30; i >= 0; i--){
+    data.push({
+      date: subDays(new Date(), i).toISOString().substr(0, 10),
+      value: (13000 + Math.random() * 5000)
+    });
+  }
+
+  console.log(data);
+
   return (
-    <div className="App bg-slate-100 min-w-full min-h-screen pt-10 pb-10">
-      <div className="bg-white w-full max-w-5xl p-6 ml-auto mr-auto">
+    <div className="App bg-slate-100 min-w-full min-h-screen p-10">
+      <div className="bg-white w-full max-w-6xl p-6 ml-auto mr-auto balance-card">
         <div>
           <div className='flex p-4'>
             <div className='flex flex-col'>
               <span className='text-xl font-extralight balance-title'> Balance </span>
               <div className='mt-4 flex justify-center items-center'>
-                <span className='font-normal mr-4 balance-amount'> $150,000 </span>
+
+                <span className='font-normal mr-4 balance-amount'> 
+                    ${Number(data[data.length - 1].value.toFixed(2)).toLocaleString('en', {minimumFractionDigits: 2, minimumFractionDigits: 2})} 
+                </span>
                 <ToggleEye />
-                
+
               </div>
             </div>
             <div className='ml-auto'>
@@ -21,6 +37,7 @@ function App() {
             </div>
           </div>
         </div>
+        <Graph data={data}/>
       </div>
     </div>
   );
